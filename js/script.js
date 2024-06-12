c22f258bc194816e6278f532559065fa55ce4fac
Dami-Bombara <bombaradamian@gmail.com>
2024-06-12 16:03:03 +0200

    Add button to specify grid, move code to functions

--- js/script.js
+++ js/script.js
@@ -1,15 +1,38 @@
+let divs = [];
+
 const container = document.querySelector("#container");
 
 const square = document.createElement("div");
 square.classList.add("square");
-square.style.width = "90px";
-square.style.height = "90px";
-square.style.border = "thin solid black";
 
+const button = document.querySelector("#set-grid");
+button.addEventListener("click", function() { grid = prompt("Please select your grid: ")});
+button.addEventListener("click", function() { deleteGrid() });
+button.addEventListener("click", function() { drawGrid(grid) });
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        let clone = square.cloneNode(true);
-        container.appendChild(clone);
+function changeColor(index) {
+    divs[index].style.backgroundColor = "red";
+}
+
+function addListeners() {
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].addEventListener("mouseover", function() { changeColor(i); });
     }
+}
+
+function deleteGrid() {
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].remove();
+    }
+}
+
+function drawGrid(grid) {
+    for (let i = 0; i < grid; i++) {
+        for (let j = 0; j < grid; j++) {
+            let clone = square.cloneNode(true);
+            divs.push(clone);
+            container.appendChild(clone);
+        }
+    }
+    addListeners();
 }
